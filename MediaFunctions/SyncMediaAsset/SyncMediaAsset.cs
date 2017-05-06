19ecbb5903344451ec96c2be5b471dc9171aaf4c
davsamu <davsamu@DESKTOP-LVNF3C6>
2017-05-06 23:21:53 +0100

    Added fetch attributes

--- MediaFunctions/SyncMediaAsset/SyncMediaAsset.cs
+++ MediaFunctions/SyncMediaAsset/SyncMediaAsset.cs
@@ -109,6 +109,7 @@ namespace MediaFunctions
                 //{
 
                 CloudBlockBlob blob = assetContainer.GetBlockBlobReference(asset.Name);
+                blob.FetchAttributes();
                 log.Info($"blobs retrieved {blob.Uri} {blob.Name} {blob.Properties.Length}");
                 
                 var assetFile = asset.AssetFiles.Create(blob.Name);
