f87b61e28e5b7463132520cc2c29949a5826f57b
davsamu <davsamu@DESKTOP-LVNF3C6>
2017-05-06 12:23:12 +0100

    Try and associate source file to minimise storage

--- MediaFunctions/CreateMediaAsset/CreateMediaAsset.cs
+++ MediaFunctions/CreateMediaAsset/CreateMediaAsset.cs
@@ -87,17 +87,17 @@ namespace MediaFunctions
 
                 // Associate asset file  
                 var assetFile = newAsset.AssetFiles.Create(sourceBlob.Name);
-                ICloudBlob destinationBlob = destAssetContainer.GetBlockBlobReference(assetFile.Name);
+                //ICloudBlob destinationBlob = destAssetContainer.GetBlockBlobReference(assetFile.Name);
 
-                // Call the CopyBlobHelpers.CopyBlobAsync extension method to copy blobs.
-                using (Task task =
-                    CopyBlobHelpers.CopyBlobAsync((CloudBlockBlob)sourceBlob,
-                        (CloudBlockBlob)destinationBlob,
-                        new BlobRequestOptions(),
-                        CancellationToken.None))
-                {
-                    task.Wait();
-                }
+                //// Call the CopyBlobHelpers.CopyBlobAsync extension method to copy blobs.
+                //using (Task task =
+                //    CopyBlobHelpers.CopyBlobAsync((CloudBlockBlob)sourceBlob,
+                //        (CloudBlockBlob)destinationBlob,
+                //        new BlobRequestOptions(),
+                //        CancellationToken.None))
+                //{
+                //    task.Wait();
+                //}
 
                 assetFile.ContentFileSize = (sourceBlob as ICloudBlob).Properties.Length;
                 assetFile.Update();
